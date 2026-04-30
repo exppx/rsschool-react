192490f77fee90e55720eb0b371c89967cb6a9f2
Gleb Morin <moringleb183@gmail.com>
2026-04-30 10:02:18 +0500

    feat: add saving last request in localStorage

--- src/features/news/SearchNewsForm/SearchNewsForm.tsx
+++ src/features/news/SearchNewsForm/SearchNewsForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { REQUEST_KEY } from '@/constants/localStorageKeys';
 import { MAX_INPUT_LENGTH } from '@/constants/numbers';
 import { TEXT } from '@/constants/text';
 import { Button } from '@ui/Button';
@@ -7,15 +8,42 @@ import { Input } from '@ui/Input';
 import searchIcon from '@/assets/search.svg';
 import styles from './SearchNewsForm.module.scss';
 
-class SearchPokemonForm extends React.Component {
+type SearchPokemonFormProps = Record<string, never>;
+
+type SearchPokemonFormState = {
+  searchRequest: string;
+};
+
+class SearchPokemonForm extends React.Component<SearchPokemonFormProps> {
+  constructor(props: SearchPokemonFormProps) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  state: SearchPokemonFormState = {
+    searchRequest: localStorage.getItem(REQUEST_KEY) ?? '',
+  };
+
+  onSubmit(event: React.SubmitEvent) {
+    event.preventDefault();
+
+    const clearSearchRequest = this.state.searchRequest.trim();
+
+    localStorage.setItem(REQUEST_KEY, clearSearchRequest);
+  }
+
   render() {
     return (
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={this.onSubmit}>
         <Input
           name="search"
           placeholder={TEXT.features.news.searchPlaceholder}
           maxLength={MAX_INPUT_LENGTH}
+          value={this.state.searchRequest}
+          onChange={(e) => this.setState({ searchRequest: e.target.value })}
         />
+
         <Button type="submit">
           <img className={styles.buttonIcon} src={searchIcon} />
         </Button>
