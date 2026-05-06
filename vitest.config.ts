bc620949e66ae57ccf284b42be01975051eb0155
Gleb Morin <moringleb183@gmail.com>
2026-05-06 11:55:42 +0500

    feat: setup Vitest, @testing-library and jsdom

---
+++ vitest.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    coverage: {
+      reporter: 'text',
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/**/index.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@news': path.resolve(__dirname, './src/features/news'),
+      '@ui': path.resolve(__dirname, './src/features/ui'),
+    },
+  },
+});
