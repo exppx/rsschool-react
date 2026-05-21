d9547660ee474c22c31883e176a04da055222c2c
Gleb Morin <moringleb183@gmail.com>
2026-05-21 17:55:41 +0500

    feat: configure Redux store with Redux Toolkit

---
+++ src/features/news/store/newsSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+export interface NewsState {
+  selectedIds: string[];
+}
+
+const initialState: NewsState = {
+  selectedIds: [],
+};
+
+const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  reducers: {
+    toggleSelectNews: (state, action: PayloadAction<string>) => {
+      const toggledNewsIndex = state.selectedIds.findIndex(
+        (id) => id === action.payload
+      );
+
+      if (toggledNewsIndex !== -1) {
+        state.selectedIds.splice(toggledNewsIndex, 1);
+      } else {
+        state.selectedIds.push(action.payload);
+      }
+    },
+  },
+});
+
+export const { toggleSelectNews } = newsSlice.actions;
+
+export default newsSlice.reducer;
